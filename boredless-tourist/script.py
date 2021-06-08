c21477b22e0fbf22201b61fe82313a92acc07796
Yash Pal Singh <yashpalsingh3737@gmail.com>
2021-06-08 13:30:26 +0530

    Added test objects

---
+++ boredless-tourist/script.py
@@ -0,0 +1,6 @@
+#List of destinations a tourist wants to travel
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical sites', 'art']]
+
